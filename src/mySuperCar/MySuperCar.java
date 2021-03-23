d3017d95900ed19716a7822af7cdabe733edbab9
sowmyagavini <sowmyagavini0023@gmail.com>
2021-03-24 02:38:09 +0530

    mySuperCar collection

---
+++ src/mySuperCar/MySuperCar.java
@@ -0,0 +1,25 @@
+package mySuperCar;
+
+import java.util.Scanner;
+
+public class MySuperCar {
+
+	public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter a super car name:");
+String supercar1=scan.nextLine();
+
+System.out.println("Enter a super car name:");
+String supercar2=scan.nextLine();
+
+System.out.println("Enter a super car name:");
+String supercar3=scan.nextLine();
+
+System.out.println("Here are all the super cars:");
+System.out.println(supercar1);
+System.out.println(supercar2);
+System.out.println(supercar3);
+
+	}
+
+}
